b46e360f08ad6500c5a61eae7d66696900f1750e
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-07-31 16:30:41 +0300

    complited

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        String num = String.valueOf(Math.abs(t));
+        int sum = 0;
+        for(int i = 0; i < num.length(); i++){
+            sum = sum + Character.getNumericValue(num.charAt(i));
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum = 0;
+        int num = 9;
+        for (int i = 0; i <= lengthOfLastNumber; i++){
+            num += Math.pow(10, i);
+            sum = sum + num;
+        }
+        System.out.println(sum);
     }
 }
